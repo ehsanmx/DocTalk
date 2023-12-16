2a06fc25537c4ed54b29e7539d1cb06a813532ae
Ehsanmx <ehsanmx@gmail.com>
2023-12-16 03:04:35 +0100

    code cleanup

---
+++ src/app.py
@@ -0,0 +1,130 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.llms import LlamaCpp
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from llama_cpp import Llama
+from langchain.prompts import PromptTemplate
+
+class DocTalkApp:
+    model = "llama-2-7b-chat.Q5_K_M.gguf"
+    # llm = Llama(model_path=f"model/{model}", chat_format="llama-2")
+    print("Render")
+    template = """[INST] <<SYS>>
+    You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+    <</SYS>>
+
+    {history}
+
+    Human:{question}
+    Assistant:
+    [/INST]
+    """
+    prompt_template = PromptTemplate.from_template(template=template)
+
+    def __init__(self):
+        self.main()
+
+    def main(self):        
+        self.init_ui()
+        st.sidebar.markdown(f"* Loading the model {self.model} is complete.")
+        self.init_pdf_uploader()
+
+    def init_ui(self):
+        with st.sidebar:
+            st.title('DocTalk 1.0')
+            st.write('Made with ♥️ by [Ehsan Zanjani](https://www.linkedin.com/in/ezanjani/)')
+            st.markdown('## Logs')
+    
+    def init_pdf_uploader(self):
+        uploaded_file = st.file_uploader('Upload your PDF file', type='pdf')
+        if uploaded_file is not None:
+            st.info("Your file is uploaded successfully!", icon='ℹ️')
+        if st.button("Analayze the file"):
+            st.markdown("""
+            ```
+                    Start Analyzing the file ...
+            ```
+                        """)
+            db = self.create_embedding_db(pdf=uploaded_file)
+            # TODO Fix this
+            docs = db.similarity_search(query="how many amendments does constitution have?", k=3)
+            st.markdown(f"""
+            ```
+                        {docs}     
+            ```
+                        """)
+            # llm_gen(docs)
+    
+    def create_embedding_db(self, pdf: PdfReader) -> FAISS:
+        pdf_reader = PdfReader(pdf)
+        text = '';
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap  = 200,
+            length_function = len)
+        chunks = text_splitter.split_text(text=text)
+        
+        embedding = HuggingFaceEmbeddings(
+            model_name = "intfloat/e5-base-v2",
+            model_kwargs = {'device': 'cpu'})
+
+        return FAISS.from_texts(texts=chunks, embedding=embedding)            
+
+
+    # Function: llm_gen
+    def llm_gen(docs):
+        """
+        Docstring for llm_gen.
+        """
+        llm = LlamaCpp(
+            model_path=f"model/{model}",
+            temperature=0.75,
+            max_tokens=4096,
+            n_ctx=4096,
+            top_p=1,
+            streaming=True,
+            callbacks=[StreamingStdOutCallbackHandler()],
+            verbose=True,  # Verbose is required to pass to the callback manager
+        )
+
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+        
+        prompt = st.chat_input("Say something")
+        if prompt:
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            st.session_state.messages.append({"role":"user","content": prompt})
+            with st.chat_message("assistant"):
+                with st.spinner('Thinking...'):
+                    full_response = []
+                    placeholder = st.empty()
+                    formatted_prompt = prompt_template.format(history=docs, question=prompt)
+                    for wordstream in llm.stream(formatted_prompt):
+                        if wordstream:
+                            full_response.append(wordstream)
+                            result = "".join(full_response).strip()
+                            placeholder.markdown(result)
+
+                    st.session_state.output_text = "".join(full_response).strip()
+                    st.toast("Processing complete!", icon='✅')
+                    st.spinner('Complete')
+        
+            st.session_state.messages.append({"role":"assistant","content": result})
+
+
+
+# Running the app
+if __name__ == "__main__":
+   dt = DocTalkApp()
